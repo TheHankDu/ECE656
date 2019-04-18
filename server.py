2c4f3c51419511cf71f902608f299458cf63afca
hankdu <r23du@uwaterloo.ca>
2019-04-18 17:40:57 -0400

    added cleaning procedure for review

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import socket
 import sys
 import struct
 import pandas as pd
+import pymysql
  
  
 SEND_BUF_SIZE = 256
@@ -11,6 +12,49 @@ RECV_BUF_SIZE = 256
 Communication_Count: int = 0
  
 receive_count : int = 0
+
+class MysqlHelper:
+    def __init__(self, host = 'localhost', user = 'root', password = 'root', database = 'proj656', charset = 'utf8'):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+        self.charset = charset
+        self.db = None
+        self.curs = None
+
+    # connect
+    def open(self):
+        self.db = ps.connect(host=self.host, user=self.user, password=self.password,database=self.database, charset=self.charset)
+        self.curs = self.db.cursor()
+
+    # connect close
+    def close(self):
+        self.curs.close()
+        self.db.close()
+
+    # modify
+    def cud(self, sql, params):
+        self.open()
+        try:
+            self.curs.execute(sql, params)
+            self.db.commit()
+            print("ok")
+        except:
+            print('modify error')
+            self.db.rollback()
+        self.close()
+
+    # search
+    def find(self, sql, params):
+        self.open()
+        try:
+            result = self.curs.execute(sql, params)
+            self.close()
+            results = self.curs.fetchall()
+            return results
+        except:
+            print('find error')
  
  
 def start_tcp_server(ip, port):
@@ -41,7 +85,7 @@ def start_tcp_server(ip, port):
     # start listening, allow only one connection
     try:
         sock.listen(1)
-    except socket.error:
+    except socket.error as e:
         print("fail to listen on port %s" % e)
         sys.exit(1)
     while True:
@@ -74,14 +118,29 @@ def start_tcp_server(ip, port):
     print(" close client connect ")
 
 def revert():
+    #Revert from backup table
 
 def clean():
+    # 1. Create New Table called temp to store all the change 
+    # 2. Peroform Data Clean
+    # 3. Commit change and update table if success
+    # 4. Delete temp if failed 
+
+    mh = MysqlHelper('localhost', 'root', 'root', 'proj656', 'utf8')
+    sql = "DELETE {0} FROM {0} INNER JOIN (SELECT {0}.business_id FROM {0} LEFT JOIN business ON {0}.business_id = business.id WHERE business.id IS NULL) as tmp on {0}.business_id = tmp.business_id".format('review')
+    results = mh.cud(sql)
+
+
+
     #identify forms of consistency and sanity checking
     #determine if there are problems with portions of data using query
     #implement solution such as ignore and create new table for analysis, or adjusting analysis in order to compensate for data skew(long tail of data distribution)
     #parameter should include threshold and identified by client
-    if True: # not as expected 
+    if False: # not as expected 
+        mh.db.rollback()
         revert()
+
+
 
 #Analyze Data include detailed and careful study of particular things 
 def analyze():
