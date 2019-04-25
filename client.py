25a95069a8a31697a9fcdaa078446e59a76d747c
hankdu <r23du@uwaterloo.ca>
2019-04-25 17:22:48 -0400

    added timeout for connection

--- client.py
+++ client.py
@@ -2,55 +2,13 @@ import socket
 import sys
  
 receive_count = 0
-import pymysql as ps
-
-class MysqlHelper:
-	def __init__(self, host = 'localhost', user = 'root', password = 'root', database = 'proj656', charset = 'utf8'):
-		self.host = host
-		self.user = user
-		self.password = password
-		self.database = database
-		self.charset = charset
-		self.db = None
-		self.curs = None
-
-	# connect
-	def open(self):
-		self.db = ps.connect(host=self.host, user=self.user, password=self.password,database=self.database, charset=self.charset)
-		self.curs = self.db.cursor()
-
-	# connect close
-	def close(self):
-		self.curs.close()
-		self.db.close()
-
-	# modify
-	def cud(self, sql, params):
-		self.open()
-		try:
-			self.curs.execute(sql, params)
-			self.db.commit()
-			print("ok")
-		except:
-			print('modify error')
-			self.db.rollback()
-			self.close()
-
-	# search
-	def find(self, sql, params):
-		self.open()
-		try:
-			result = self.curs.execute(sql, params)
-			self.close()
-			results = self.curs.fetchall()
-			return results
-		except:
-			print('find error')
-			
+import pymysql as ps	
 			
 def start_tcp_client(ip, port): 
 	###create socket
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	# Set Timeout to 60 sec
+	s.settimeout(60)
 
 	failed_count = 0
 	while True:
@@ -74,23 +32,13 @@ def start_tcp_client(ip, port):
 		print("client TCP send buffer size is %d" % s_send_buffer_size)
 		print("client TCP receive buffer size is %d" % s_receive_buffer_size)
 
-		receive_count = 0
 		while True:
-			msg = 'hello server, i am the client'
-			s.send(msg.encode('utf-8'))
 			print("send len is : [%d]" % len(msg))
 			
 			msg = s.recv(1024)
 			print(msg.decode('utf-8'))
 			print("recv len is : [%d]" % len(msg))
 
-			receive_count += 1
-
-			if receive_count == 14:
-				msg = 'disconnect'
-				print("total send times is : %d " % receive_count)
-				s.send(msg.encode('utf-8'))
-				break
 		break
 	s.close()
 
