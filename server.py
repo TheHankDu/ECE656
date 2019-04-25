5b38a1dd708ca137da65938b740be645272462e5
hankdu <r23du@uwaterloo.ca>
2019-04-25 17:23:43 -0400

    added timeout for connection add client reply modified data cleaning

--- server.py
+++ server.py
@@ -2,21 +2,21 @@ import socket
 import sys
 import struct
 import pymysql
+import tree
 
 #from apyori import apriori
 import numpy as np
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
+# from sklearn.tree import DecisionTreeClassifier
+# from sklearn.model_selection import train_test_split
+# from sklearn.metrics import accuracy_score 
+# from sklearn.metrics import classification_report 
+# from sklearn.metrics import confusion_matrix 
 
-client 
-SEND_BUF_SIZE = 256
- 
-RECV_BUF_SIZE = 256
- 
-Communication_Count: int = 0
- 
-receive_count : int = 0
+client = None
+cleaned_data = None
+
+BUF_SIZE = 256
 
 class MysqlHelper:
     def __init__(self, host = 'localhost', user = 'root', password = 'root', database = 'lahman2016', charset = 'utf8'):
@@ -44,13 +44,13 @@ class MysqlHelper:
         try:
             rowCount = self.curs.execute(sql)
             if(rowCount > 0):
-                print("Effected Row:".format{rowCount})
+                client.send("Effected Row:".format{rowCount})
             self.close()
         except ps.MySQLError as e:
             print('modify error')
+            client.send("")
             self.close()
             return e
-            #TODO: Notify client
         
     # search
     def find(self, sql):
@@ -71,7 +71,16 @@ class MysqlHelper:
         sql = "SELECT {0}.{2} FROM {0} LEFT JOIN {1} ON {0}.{2} = {1}.{2} WHERE {1}.{2} IS NULL".format(firstTable,secondTable,idName)
         results = self.find(sql)
         if(len(results) > 0):
-            return results
+            client.send('There are {0} entries that exist in {1} but not in {2} for {3}. Please reply 1 to delete or complete SQL query to Adjust it'.format(len(results),firstTable,secondTable,idName))
+            reply = client.recv(16384)
+            if(reply == '1'):
+                sql = "DELETE {0} FROM {0} INNER JOIN (SELECT {0}.{2} FROM {0} LEFT JOIN {1} ON {0}.{2} = {1}.{2} WHERE {1}.{2} IS NULL) as tmp on {0}.{2} = tmp.{2}".format(firstTable,secondTable,idName)
+                self.cud(sql)
+            else:
+                sql = client.recv(16384)
+                #sql = UPDATE HallOfFame SET playerID='drewjd01' WHERE playerID='drewj.01'
+                self.cud(sql)
+
 
     def add_index(self,table,indexName):
         sql = "ALTER TABLE {0} ADD INDEX ({1});".format(table,indexName)
@@ -83,27 +92,18 @@ def start_tcp_server(ip, port):
     # create socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_address = (ip, port)
+
+    # Set Timeout to 60 sec
+    s.settimeout(60)
  
     # bind port
     print("starting listen on ip %s, port %s" % server_address)
     sock.bind(server_address)
  
-    # get the old receive and send buffer size
-    s_send_buffer_size = sock.getsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF)
-    s_recv_buffer_size = sock.getsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF)
-    print("socket send buffer size[old] is %d" % s_send_buffer_size)
-    print("socket receive buffer size[old] is %d" % s_recv_buffer_size)
- 
     # set a new buffer size
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, SEND_BUF_SIZE)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, RECV_BUF_SIZE)
- 
-    # get the new buffer size
-    s_send_buffer_size = sock.getsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF)
-    s_recv_buffer_size = sock.getsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF)
-    print("socket send buffer size[new] is %d" % s_send_buffer_size)
-    print("socket receive buffer size[new] is %d" % s_recv_buffer_size)
- 
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, BUF_SIZE)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, BUF_SIZE)
+
     # start listening, allow only one connection
     try:
         sock.listen(1)
@@ -119,17 +119,33 @@ def start_tcp_server(ip, port):
 
     while True:
         msg = client.recv(16384)
-        msg_de = msg.decode('utf-8')
+        msg_de = msg
         
         if msg_de == '4':
             print("Client Requested to Disconnect, Disconnecting...")
             break
-
         ##############################################
         #Run Procedure HERE
+        elif msg_de == '1':
+            if(client.recv(16384) == 'Y')
+                commit = True
+                role = client.recv(16384)
+                period = client.recv(16384)
+                clean(commit,role,period)
+            else:
+                role = client.recv(16384)
+                period = client.recv(16384)
+        elif msg_de == '2':
+            analyze()
+        elif msg_de == '3':
+            validate()
+        else:
+            client.send('unknown option')
+
+
         ##############################################
  
-        client.send(msg.encode('utf-8'))
+        client.send(msg)
  
     client.close()
     sock.close() 
@@ -160,28 +176,20 @@ def clean(commit = False,role,period):
     results = mh.consist_check('Fielding','Master','playerID')
     results = mh.consist_check('HallOfFame','Master','playerID')
 
-    results = mh.consist_check('user_elite','Batting','yearID')
-    results = mh.consist_check('user_friends','user','yearID')
+    results = mh.consist_check('Pitching','Batting','yearID')
+    results = mh.consist_check('Fielding','Batting','yearID')
     results = mh.consist_check('tip','user','yearID')
     results = mh.consist_check('review','user','yearID')
 
     
-    sql = "DELETE FROM Master WHERE birthYear == ''"
+    sql = "DELETE FROM Master WHERE finalGame = '' OR debut = ''"
     results = mh.cud(sql)
-    sql = "DELETE FROM HallOfFame WHERE inducted = 'N'"
+    # if a player is not inducted and vote is less than 5%, he is out of the pool
+    sql = "DELETE FROM HallOfFame WHERE inducted = 'N' AND votes/ballots < 0.05"
     results = mh.cud(sql)
     #since player is not eligible if not retired for at least 5 years or they do not meet ten year rule
-    sql = "DELETE FROM Master WHERE finalGame > '2011-12-31' OR finalGame-debut<10"
-
-    sql = 'select * FROM HallOfFame where not exists (select playerID from Master where playerID = HallOfFame.playerID);'
+    sql = "DELETE FROM Master WHERE finalGame > '2011-12-31' OR finalGame-debut < 10"
     results = mh.cud(sql)
-    if(results != emptyset):
-        #ask user for the choices: delete or update
-        #if delete
-            #sql = "DELETE FROM {0} WHERE playerID = {0}".format('HallOfFame',results)
-        #elif update
-            #sql = UPDATE HallOfFame SET playerID='drewjd01' WHERE playerID='drewj.01';
-
 
     ################################################
 
@@ -189,6 +197,10 @@ def clean(commit = False,role,period):
     #determine if there are problems with portions of data using query
     #implement solution such as ignore and create new table for analysis, or adjusting analysis in order to compensate for data skew(long tail of data distribution)
     #parameter should include threshold and identified by client
+
+    # playerID,Career Win, Career ShoutOut, Career StrikeOut,Career Hits,Career HomeRun,Career RBI (Runs Batted In), Career OBP(On base percentage), Career All Star Appearence
+    sql = Select playerID,tot_W,tot_SHO,tot_SO,tot_H,tot_RBI,OBP,ASA from ((Select playerID,sum(W) as tot_W,sum(SHO) as tot_SHO,sum(SO) as tot_SO from Pitching group by (playerID)) inner join (select playerID,sum(H) as tot_H,sum(RBI) as tot_RBI sum(H+BB+HBP)/sum(AB+BB+SF+HBP) as OBP from Batting group by (playerID)) using (playerID) inner join (select playerID,sum(GP) as asa from AllstarFull group by (playerID)) using (playerID);
+    cleaned_data = mh.find(sql)
 
     if commit: # not as expected 
         mh.db.commit()
@@ -199,8 +211,9 @@ def clean(commit = False,role,period):
 
 # Predict who will be inducted into Hall of Fame
 def analyze():
-    print("TODO")
-
+    dt = tree.DecisionTreeClass()
+    tree.fit(cleaned_data,result)
+    # Stats to be considered: Wins(W),Losses(L),Strike-out(SO),Hits(H),Homer(HR),All Start Appearence count,
     # Minimized return to Client, only the result
     
 
