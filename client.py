2d9c3786b21fc0951fce77b931b23794ecbf99fa
hankdu <r23du@uwaterloo.ca>
2019-04-26 17:14:20 -0400

    Life is Hard data clean accomplished Connection seems to be working commit rollback need to be adjusted

--- client.py
+++ client.py
@@ -2,24 +2,22 @@ import socket
 import sys
 import time
  
-receive_count = 0
 import pymysql as ps	
 			
-s = None
 isClean = False
 isAnalyzed = False
 
 def start_tcp_client(ip, port): 
 	###create socket
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	# Set Timeout to 60 sec
-	s.settimeout(60)
+	#server.settimeout(60)
 
 	failed_count = 0
 	while True:
 		try:
 			print("start connect to server ")
-			s.connect((ip, port))
+			server.connect((ip, port))
 			break
 		except socket.error:
 			failed_count += 1
@@ -27,23 +25,33 @@ def start_tcp_client(ip, port):
 			print("fail to connect to server %d times" % failed_count)
 			if failed_count == 10: return
 
+	quit = False
 	# send and receive
-	#get the socket send buffer size and receive buffer size
-	s_send_buffer_size = s.getsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF)
-	s_receive_buffer_size = s.getsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF)
-
-	while True:
-		quit = create_menu()
+	while (not quit):
+		create_menu()
+		tmp = input()
+		if tmp == '1':
+			if(clean(server) == '4'):
+				quit = True
+		elif tmp == '2':
+			analyze(server)
+		elif tmp == '3':
+			validate(server)
+		elif (tmp == 'R' or tmp == 'r'):
+			revert(server)
+		elif (tmp == '4' or rec == '4'):
+			quit = True
+		else:
+			print("Invalid Option, Please enter your option again")
+			#print("You should not be here, Report the detail how you reach this line to my lovely cat")
 		if(quit):
 			break
-		print("Waiting for Server Response...")
-		rec = s.recv(1024).decode()
-		print(rec)
-	s.close()
+	server.close()
 
 #Clean Data
 #Parameter includes database name, table name,all the parameter required...
-def clean():
+def clean(s):
+	global isClean
 	s.send('1'.encode())
 	print("Please Enter the Option:")
 	print("Do you want to commit the change? (Y/N)")
@@ -52,47 +60,47 @@ def clean():
 		s.send('Y'.encode())
 	else:
 		s.send('N'.encode())
-	
-	while(!isClean):
+	isClean = False
+	while(not isClean):
 		tmp = s.recv(1024).decode()
-		if(tmp = "Finished"):
+		if(tmp == "Finished"):
 			isClean = True
-		print(tmp)
+		elif(tmp == '4'):
+			return tmp
+		elif(tmp != ''):
+			print(tmp)
+			if(tmp[0] == 'O'):
+				tmp = input()
+				s.send(tmp.encode())
 	return
 
 #Analyze data
-def analyze():
-	if(!isClean):
+def analyze(s):
+	global isClean
+	if(not isClean):
 		print("Data is not cleaned, default clean has started...")
 		clean()
 	s.send('2'.encode())
 	#Check if data is cleaned
 
 #Validate analysis
-def validate():
+def validate(s):
+	if(not isAnalyzed):
+		print("Data is not analyzed, default clean has started...")
+		analyze()
 	s.send('3'.encode())
 	#Check if data is analyzed
 
+def revert(s):
+	s.send('R'.encode())
+
 
 def create_menu():
-	quit = False
 	print("1: Data Clean")
 	print("2: Data Analyze")
 	print("3: Data Validate")
+	print("R: Revert Database")
 	print("4: Quit")
-	tmp = input()
-	if tmp == '1':
-		clean()
-	elif tmp == '2':
-		analyze()
-	elif tmp == '3':
-		validate()
-	elif tmp == '4':
-		quit = True
-	else:
-		print("Invalid Option, Please enter your option again")
-		#print("You should not be here, Report the detail how you reach this line to my lovely cat")
-	return quit
 
 if __name__=='__main__':
 	start_tcp_client('127.0.0.1',6000)
--- server.py
+++ server.py
@@ -4,17 +4,12 @@ import struct
 import pymysql as ps
 import tree
 
-#from apyori import apriori
 import numpy as np
 import pandas as pd
-# from sklearn.tree import DecisionTreeClassifier
-# from sklearn.model_selection import train_test_split
-# from sklearn.metrics import accuracy_score 
-# from sklearn.metrics import classification_report 
-# from sklearn.metrics import confusion_matrix 
 
 client = None
-cleaned_data = None
+feature_list = None
+result_list = None
 
 BUF_SIZE = 256
 
@@ -39,49 +34,58 @@ class MysqlHelper:
         self.db.close()
 
     # modify
-    def cud(self, sql):
+    def cud(self, sql,client):
         self.open()
+        print(sql)
         try:
             rowCount = self.curs.execute(sql)
+            print(rowCount)
             if(rowCount > 0):
-                client.send("Effected Row:".format(rowCount).encode())
+                client.send("Effected Row:{0}".format(rowCount).encode())
             self.close()
         except ps.MySQLError as e:
             client.send("Modification Error. Code:{0} Detail:{1}".format(e.args[0],e.args[1]).encode())
             self.close()
         
     # search
-    def find(self, sql):
+    def find(self, sql,client):
         self.open()
         try:
+            results = ()
             rowCount = self.curs.execute(sql)
             if(rowCount > 0):
                 results = self.curs.fetchall()
             self.close()
+            print(results)
             return results
         except ps.MySQLError as e:
+            print("Query Error. Code:{0} Detail:{1}".format(e.args[0],e.args[1]))
             client.send("Query Error. Code:{0} Detail:{1}".format(e.args[0],e.args[1]).encode())
             self.close()
-            return e
+            raise
+        except ps.InterfaceError as ie:
+            print("Interface Error. Code:{0} Detail:{1}".format(e.args[0],e.args[1]))
         
 
-    def consist_check(self,firstTable,secondTable,idName):
+    def consist_check(self,client,firstTable,secondTable,idName):
+        print("Running Consistency Check {0} {1} {2}...".format(firstTable,secondTable,idName))
         sql = "SELECT {0}.{2} FROM {0} LEFT JOIN {1} ON {0}.{2} = {1}.{2} WHERE {1}.{2} IS NULL".format(firstTable,secondTable,idName)
-        results = self.find(sql)
+        results = self.find(sql,client)
         if(len(results) > 0):
-            client.send('There are {0} entries that exist in {1} but not in {2} for {3}. Please reply 1 to delete or complete SQL query to Adjust it'.format(len(results),firstTable,secondTable,idName).encode())
-            reply = client.recv(16384).decode()
+            client.send('O:There are {0} entries that exist in {1} but not in {2} for {3}. Please reply 1 to delete or anything to bypass it'.format(len(results),firstTable,secondTable,idName).encode())
+            reply = None
+            while(reply == None):
+                reply = client.recv(16384).decode()
+            print(reply)
             if(reply == '1'):
                 sql = "DELETE {0} FROM {0} INNER JOIN (SELECT {0}.{2} FROM {0} LEFT JOIN {1} ON {0}.{2} = {1}.{2} WHERE {1}.{2} IS NULL) as tmp on {0}.{2} = tmp.{2}".format(firstTable,secondTable,idName)
-                self.cud(sql)
-            else:
-                #ignore
-                continue
+                self.cud(sql,client)
+        print("Finished Consistency Check {0} {1} {2}".format(firstTable,secondTable,idName))                
 
-
-    def add_index(self,table,indexName):
+    def add_index(self,client,table,indexName):
+        print("Adding Index {0}".format(table))
         sql = "ALTER TABLE {0} ADD INDEX ({1});".format(table,indexName)
-        results = self.cud(sql)
+        results = self.cud(sql,client)
         return results
  
  
@@ -91,7 +95,7 @@ def start_tcp_server(ip, port):
     server_address = (ip, port)
 
     # Set Timeout to 60 sec
-    sock.settimeout(60)
+    sock.settimeout(30)
  
     # bind port
     print("starting listen on ip %s, port %s" % server_address)
@@ -110,124 +114,135 @@ def start_tcp_server(ip, port):
     
     print("waiting for connection")
     client, addr = sock.accept()
-    print("having a connection")
-        
-    msg = 'welcome to tcp server' + "\r\n"
-
-    while True:
-        msg = client.recv(16384)
-        msg_de = msg.decode()
-        print(msg_de)
-        
-        if msg_de == '4':
-            print("Client Requested to Disconnect, Disconnecting...")
-            client.send('4'.encode())
-            break
-        ##############################################
-        #Run Procedure HERE
-        elif msg_de == '1':
-            if(client.recv(16384) == 'Y'):
-                commit = True
-                clean(commit)
+    print("Connected")
+    try:
+        while True:
+            msg = client.recv(16384)
+            msg_de = msg.decode()
+            print(msg_de)
+            
+            if msg_de == '4':
+                print("Client Requested to Disconnect, Disconnecting...")
+                break
+            ##############################################
+            #Run Procedure HERE
+            elif msg_de == '1':
+                if(client.recv(16384) == 'Y'):
+                    commit = True
+                    clean(client,commit)
+                else:
+                    clean(client)
+            elif msg_de == '2':
+                period = client.recv(16384)
+                analyze(client,period)
+            elif msg_de == '3':
+                if(period == None):
+                    analyze(client)
+                validate(client)
+            elif msg_de == 'R':
+                revert(client)
             else:
-                clean()
-        elif msg_de == '2':
-            period = client.recv(16384)
-            analyze(period)
-        elif msg_de == '3':
-            if(period = None):
+                client.send('unknown option'.encode())
 
-            validate()
-        else:
-            client.send('unknown option'.encode())
-
-        ##############################################
- 
-    client.close()
-    sock.close() 
+            ##############################################
+    except:
+        print("EXCEPT EXIT")
+        print(sys.exc_info()[0], sys.exc_info()[1])
+    finally:
+        client.send('4'.encode())
+        client.close()
+        sock.close() 
     print("Disconnected")
 
-def clean(commit = False):
-    print('start cleaning')
+def clean(client,commit = False,period = 2010):
+    print('start cleaning...')
     mh = MysqlHelper()
 
     #########################################################
     # Default Cleanup Process
-    results = mh.add_index('Master','playerID')
-    results = mh.add_index('Batting','playerID')
-    results = mh.add_index('Pitching','playerID')
-    results = mh.add_index('Fielding','playerID')
-    results = mh.add_index('AllstarFull','playerID')
-    results = mh.add_index('HallOfFame','playerID')
-    results = mh.add_index('Managers','playerID')
+    results = mh.add_index(client,'Master','playerID')
+    results = mh.add_index(client,'Batting','playerID')
+    results = mh.add_index(client,'Pitching','playerID')
+    results = mh.add_index(client,'Fielding','playerID')
+    results = mh.add_index(client,'AllstarFull','playerID')
+    results = mh.add_index(client,'HallOfFame','playerID')
+    results = mh.add_index(client,'Managers','playerID')
 
-    results = mh.add_index('Batting','yearID')
-    results = mh.add_index('Pitching','yearID')
-    results = mh.add_index('Fielding','yearID')
-    results = mh.add_index('AllstarFull','yearID')
-    results = mh.add_index('Managers','yearID')
-    results = mh.add_index('HallOfFame','yearID')
+    results = mh.add_index(client,'Batting','yearID')
+    results = mh.add_index(client,'Pitching','yearID')
+    results = mh.add_index(client,'Fielding','yearID')
+    results = mh.add_index(client,'AllstarFull','yearID')
+    results = mh.add_index(client,'Managers','yearID')
+    results = mh.add_index(client,'HallOfFame','yearID')
+
+    print('Finished Adding Index')
 
     #########################################################
     #Consistancy Check
-    results = mh.consist_check('Batting','Master','playerID')
-    results = mh.consist_check('Pitching','Master','playerID')
-    results = mh.consist_check('Fielding','Master','playerID')
-    results = mh.consist_check('HallOfFame','Master','playerID')
+    results = mh.consist_check(client,'Batting','Master','playerID')
+    results = mh.consist_check(client,'Pitching','Master','playerID')
+    results = mh.consist_check(client,'Fielding','Master','playerID')
+    results = mh.consist_check(client,'HallOfFame','Master','playerID')
 
+    print('Finished Consistancy Check')
     #########################################################
     # Sanity Check
     sql = "DELETE FROM Master WHERE finalGame = '' OR debut = ''"
-    results = mh.cud(sql)
+    results = mh.cud(sql,client)
     # if a player is not inducted and vote is less than 5%, he is out of the pool
     sql = "DELETE FROM HallOfFame WHERE inducted = 'N' AND votes/ballots < 0.05"
-    results = mh.cud(sql)
+    results = mh.cud(sql,client)
+    sql = "UPDATE HallOfFame SET playerID='drewjd01' WHERE playerID='drewj.01'"
+    results = mh.cud(sql,client)
     #since player is not eligible if not retired for at least 5 years or they do not meet ten year rule
     sql = "DELETE FROM Master WHERE finalGame > '2011-12-31' or LEFT(finalGame,4)-LEFT(debut,4) < 10;"
-    results = mh.cud(sql)
+    results = mh.cud(sql,client)
     #Delete non-inducted record if the player was inducted
     sql = "DELETE t1 FROM HallOfFame t1, HallOfFame t2 WHERE t1.inducted < t2.inducted and t1.playerID = t2.playerID;"
-    results = mh.cud(sql)
-
+    results = mh.cud(sql,client)
+    print('Finished Sanity Check')
     ##########################################################
 
-    #identify forms of consistency and sanity checking
-    #determine if there are problems with portions of data using query
     #implement solution such as ignore and create new table for analysis, or adjusting analysis in order to compensate for data skew(long tail of data distribution)
-    #parameter should include threshold and identified by client
 
-    # playerID,Career Win, Career ShoutOut, Career StrikeOut,Career Hits,Career HomeRun,Career RBI (Runs Batted In), Career OBP(On base percentage), Career All Star Appearence
-    sql = 'CREATE VIEW TrainSet AS SELECT playerID,ifnull(tot_W,0) as tot_W,ifnull(tot_SHO,0)as tot_SHO,ifnull(tot_SO,0) as total_SO,tot_H,tot_HR,tot_RBI,ifnull(OBP,0) as total_OBP,ifnull(ASA,0)as ASA from ((Select playerID, sum(W) as tot_W, sum(SHO) as tot_SHO, sum(SO) as tot_SO from Pitching group by (playerID))as t1 right join (select playerID,sum(H) as tot_H,sum(HR) as tot_HR, sum(RBI) as tot_RBI, sum(H+BB+HBP)/sum(AB+BB+SF+HBP) as OBP from Batting group by (playerID))as t2 using (playerID) left join (select playerID,sum(GP) as asa from AllstarFull group by (playerID)) as t3 using (playerID));'
+    # Get Train Set
+    # playerID,Career Win, Career ShoutOut, Career StrikeOut,Career Hits,Career HomeRun,Career RBI (Runs Batted In), Career OBP(On base percentage), Career All Star Appearence, lastyear
+    sql = 'DROP VIEW IF EXISTS TrainSet;'
+    results = mh.cud(sql,client)
+    sql = 'CREATE VIEW TrainSet AS SELECT playerID, IFNULL(tot_W, 0) AS tot_W, IFNULL(tot_SHO, 0) AS tot_SHO,IFNULL(tot_SO, 0) AS total_SO,tot_H,tot_HR,tot_RBI,IFNULL(OBP, 0) AS total_OBP,IFNULL(ASA, 0) AS ASA,lastyear FROM((SELECT playerID, SUM(W) AS tot_W, SUM(SHO) AS tot_SHO, SUM(SO) AS tot_SO FROM Pitching GROUP BY (playerID)) AS t1 RIGHT JOIN (SELECT playerID, SUM(H) AS tot_H, SUM(HR) AS tot_HR, SUM(RBI) AS tot_RBI, SUM(H + BB + HBP) / SUM(AB + BB + SF + HBP) AS OBP FROM Batting GROUP BY (playerID)) AS t2 USING (playerID) LEFT JOIN (SELECT playerID, SUM(GP) AS asa FROM AllstarFull GROUP BY (playerID)) AS t3 USING (playerID) LEFT JOIN (SELECT playerID, LEFT(finalGame, 4) AS lastyear FROM Master) AS t4 USING (playerID));'
+    #sql = 'CREATE VIEW TrainSet AS SELECT playerID,ifnull(tot_W,0) as tot_W,ifnull(tot_SHO,0)as tot_SHO,ifnull(tot_SO,0) as total_SO,tot_H,tot_HR,tot_RBI,ifnull(OBP,0) as total_OBP,ifnull(ASA,0)as ASA from ((Select playerID, sum(W) as tot_W, sum(SHO) as tot_SHO, sum(SO) as tot_SO from Pitching group by (playerID))as t1 right join (select playerID,sum(H) as tot_H,sum(HR) as tot_HR, sum(RBI) as tot_RBI, sum(H+BB+HBP)/sum(AB+BB+SF+HBP) as OBP from Batting group by (playerID))as t2 using (playerID) left join (select playerID,sum(GP) as asa from AllstarFull group by (playerID)) as t3 using (playerID));'
+    results = mh.cud(sql,client)
 
-        # Select playerID,tot_W,tot_SHO,tot_SO,tot_H,tot_RBI,OBP,ASA from ((Select playerID,sum(W) as tot_W,sum(SHO) as tot_SHO,sum(SO) as tot_SO from Pitching group by (playerID)) inner join (select playerID,sum(H) as tot_H,sum(RBI) as tot_RBI sum(H+BB+HBP)/sum(AB+BB+SF+HBP) as OBP from Batting group by (playerID)) using (playerID) inner join (select playerID,sum(GP) as asa from AllstarFull group by (playerID)) using (playerID);
-    cleaned_data = mh.find(sql)
+    sql = 'SELECT * FROM TrainSet;'
+    feature_list = mh.find(sql,client)
 
-    if commit: # not as expected 
-        mh.db.commit()
-    else:
-        mh.db.rollback()
+
+    # sql = "SELECT playerID,ifnull(inducted,'N') AS inducted,yearid FROM TrainSet Left JOIN HallOfFame USING (playerID) WHERE yearid < {0};".format(period)
+    # result_list = mh.find(sql,client)
+    
+    # if commit: # not as expected 
+    #     mh.db.commit()
+    # else:
+    #     mh.db.rollback()
+
+    print("Finished Cleanup")
     client.send("Finished".encode())
 
-
-
 # Predict who will be inducted into Hall of Fame
-def analyze(period):
-    if(cleaned_data == None):
+def analyze(client,period):
+    global feature_list
+    global result_list
+    if(feature_list == None or result_list == None):
         client.send("data is not cleaned yet, please clean data first")
         return
 
-    mh = MysqlHelper()
-
-    sql = "SELECT playerID,ifnull(inducted,'N') AS inducted,yearid FROM TrainSet Left JOIN HallOfFame USING (playerID) WHERE yearid < {period};".format(period)
-    result = mh.find(sql)
-
     detree = DecisionTreeClass()
-    detree.fit(cleaned_data,result)
+    detree.fit(feature_list,result_list)
     # Stats to be considered: Wins(W),Losses(L),Strike-out(SO),Hits(H),Homer(HR),All Start Appearence count,
     # Minimized return to Client, only the result
     client.send("Finished".encode())
 
-def validate():
+def validate(client):
     #divide data into two at random.
     #first half would be used to analysis and predict for the other half
     #The other half would be used to validate or refute hypothesis
@@ -235,9 +250,14 @@ def validate():
     print("TODO")
     client.send("Finished".encode())
 
+def revert(client):
+    print("TODO")
+
 
  
  
  
 if __name__=='__main__':
-    start_tcp_server('127.0.0.1',6000)
+        start_tcp_server('127.0.0.1',6000)
+
+
